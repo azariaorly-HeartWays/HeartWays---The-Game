a32203c627bba7708485a3b7095e6b95cb31371a
azariaorly-HeartWays <azaria.orly@gmail.com>
2025-08-15 04:50:57 +0300

    Delete crs directory

--- crs/components/src/components/BackToBe.tsx
+++
@@ -1,5 +0,0 @@
-import { useNavigate } from 'react-router-dom'
-export default function BackToBe(){
-  const nav = useNavigate()
-  return <button className="btn" onClick={()=>nav('/be')}>⬅ חזרה ל-Be</button>
-}
